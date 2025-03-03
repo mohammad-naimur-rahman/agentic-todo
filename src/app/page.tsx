a724f1c3bcb530b2514f80dcd412fdb546cc8e05
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-03-03 23:19:37 +0600

    config: shadcn ui add example: basic chat completion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
-export default function Home() {
-  return <h1>Hello world!</h1>
+'use client'
+
+import { Input } from '@/components/ui/input'
+import { useChat } from '@ai-sdk/react'
+import { IconRobot, IconUserCircle } from '@tabler/icons-react'
+
+export default function Homepage() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat()
+  return (
+    <div className='flex flex-col w-full max-w-lg py-24 mx-auto stretch min-h-screen'>
+      <div className='flex flex-col gap-2'>
+        {messages.map(m => (
+          <div
+            key={m.id}
+            className='whitespace-pre-wrap flex items-start gap-3'
+          >
+            {m.role === 'user' ? (
+              <IconUserCircle className='size-5 min-w-5 mt-1 text-sky-600' />
+            ) : (
+              <IconRobot className='size-5 min-w-5 mt-1 text-emerald-600' />
+            )}
+            <p className='text-gray-600 dark:text-gray-300'>{m.content}</p>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className='mt-auto'>
+        <Input
+          value={input}
+          placeholder='Say something...'
+          onChange={handleInputChange}
+          className='mt-4'
+        />
+      </form>
+    </div>
+  )
 }
